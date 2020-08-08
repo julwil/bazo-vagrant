6929d7a0ba452b066a69919a8b3228a68fc0342e
julius_willems <julius.willems@staff.cloud>
2020-08-08 16:23:51 +0200

    clean up

--- after.sh
+++ after.sh
@@ -1,6 +1,7 @@
 echo "Installing Go"
 wget https://golang.org/dl/go1.14.7.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.14.7.linux-amd64.tar.gz
+sudo chmod 777 -R go
 rm go1.14.7.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 echo PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
